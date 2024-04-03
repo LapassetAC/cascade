e772dcba0a461164bcad5fea06235a22c1001b69
AdrienLapasset <aflapasset@gmail.com>
2024-04-03 12:38:53 +0200

    reorder component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -5,17 +5,17 @@ import localFont from "next/font/local";
 const moderat = localFont({
   src: [
     {
-      path: "../assets/fonts/Moderat-Black-Italic.woff2",
+      path: "../../assets/fonts/Moderat-Black-Italic.woff2",
       weight: "900",
       style: "italic",
     },
     {
-      path: "../assets/fonts/Moderat-Black.woff2",
+      path: "../../assets/fonts/Moderat-Black.woff2",
       weight: "900",
       style: "normal",
     },
     {
-      path: "../assets/fonts/Moderat-Medium.woff2",
+      path: "../../assets/fonts/Moderat-Medium.woff2",
       weight: "500",
       style: "normal",
     },
