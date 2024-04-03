764362945176326abf5e088e706ca4ee68aaaf4d
Clément Lapasset <clement.lapasset@gmail.com>
2024-04-03 16:33:25 +0200

    clean code

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,8 @@ import styled from "styled-components";
 import { textApparitionAnim, fadeIn, cascadeDelay } from "@/styles/theme";
 
 const StyledContainer = styled.div`
-  /* background-color: ${(props) => props.$bgColor}; */
-  /* transition: background-color 0.4s; */
   section {
     &.hero {
-      /* margin: 30px 0; */
       height: calc(100vh - 180px);
       p {
         overflow-y: hidden;
@@ -22,34 +19,14 @@ const StyledContainer = styled.div`
             ${cascadeDelay(5, 0.7)}
           }
         }
-        /* &:nth-child(2) {
-          grid-column: 3 / 6;
-          opacity: 0;
-          animation: ${fadeIn} 0.4s 2s forwards;
-        } */
       }
     }
   }
 `;
 
 export default function Home({ projects }) {
-  // const [animIsLoaded, setAnimIsLoaded] = useState(false);
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setAnimIsLoaded(true);
-  //   }, 2400);
-  //   return () => clearTimeout(timer);
-  // }, []);
-
-  // const changeColors = (newBgColor, newFontColor) => {
-  //   setBgColor(newBgColor);
-  //   setFontColor(newFontColor);
-  //   document.body.style.color = newFontColor;
-  // };
-
   return (
-    <StyledContainer /*$bgColor={colors.bgColor}*/>
+    <StyledContainer>
       <section className="grid hero">
         <p>
           <span>Créateurs </span> <span>de&nbsp;</span>
