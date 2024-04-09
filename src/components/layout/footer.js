bd3de8e75f863187913fa9cb3f7273e5e11a9c96
AdrienLapasset <aflapasset@gmail.com>
2024-04-09 17:59:56 +0200

    animation css

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -2,99 +2,154 @@ import styled, { css, keyframes } from "styled-components";
 import React, { useState, useEffect, useRef } from "react";
 import theme from "@/styles/theme";
 
+const screenHeightWithoutHeader = `calc(100vh - 90px)`;
+
 const drop1 = keyframes`
 	from {
 		opacity: 0;
-    clip-path: rect(0 0px 0px 0);
+    clip-path: rect(0 0% 0% 0);
 	}
 	to {
 		opacity: 1;
-		clip-path: inset(0 100px 100px 0);
+		clip-path: rect(0 44% 13% 0);
 	}
 `;
 const drop2 = keyframes`
 	from {
 		opacity: 0;
-    clip-path: rect(0 0px 0px 0);
+    clip-path: rect(0 0% 0% 0);
 	}
 	to {
 		opacity: 1;
-		clip-path: inset(0 100px 100px 0);
+		clip-path: rect(0 56% 36% 0);
 	}
 `;
 const drop3 = keyframes`
 	from {
 		opacity: 0;
-    clip-path: rect(0 0px 0px 0);
+    clip-path: rect(0 0% 0% 0);
 	}
 	to {
 		opacity: 1;
-		clip-path: inset(0 100px 100px 0);
+		clip-path: rect(0 67% 72% 0);
 	}
 `;
 const drop4 = keyframes`
 	from {
 		opacity: 0;
-    clip-path: rect(0 0px 0px 0);
+    clip-path: rect(0 0% 0% 0);
 	}
 	to {
 		opacity: 1;
-		clip-path: inset(0 100px 100px 0);
+		clip-path: rect(0 78% 90% 0);
 	}
 `;
 const drop5 = keyframes`
 	from {
 		opacity: 0;
-    clip-path: rect(0 0px 0px 0);
+    clip-path: rect(0 0% 0% 0);
 	}
 	to {
 		opacity: 1;
-		clip-path: inset(0 100px 100px 0);
+		clip-path: rect(0 89% 99% 0);
 	}
 `;
 const drop6 = keyframes`
 	from {
-		opacity: 0;
-    clip-path: rect(0 0px 0px 0);
+    opacity: 0;
+    clip-path: rect(0 0% 0% 0);
 	}
 	to {
-		opacity: 1;
-		clip-path: inset(0 100px 100px 0);
+    opacity: 1;
+		clip-path: rect(0 100% 100% 0);
 	}
 `;
 
 const StyledFooter = styled.footer`
-  height: calc(100vh - 90px);
+  height: ${screenHeightWithoutHeader};
+  margin-top: 60px;
+  @media ${({ theme }) => theme.minWidth.sm} {
+    font-size: 16px;
+    margin-top: 90px;
+  }
   aside {
+    grid-row: 1/2;
     width: 100%;
-    height: 300px;
     background-color: ${({ theme }) => theme.color.black};
 
     view-timeline-name: --revealing-image;
     view-timeline-axis: block;
-
     animation-timeline: --revealing-image;
-
-    animation-range: entry 0% cover 100%;
-    animation-direction: both;
+    animation-range: entry 0% cover ${screenHeightWithoutHeader};
+    animation-fill-mode: both;
+    animation-timing-function: linear;
 
     &:nth-of-type(1) {
       animation-name: ${drop1};
+      animation-delay: 1s;
+      animation-duration: 6s;
     }
     &:nth-of-type(2) {
       animation-name: ${drop2};
+      animation-delay: 2s;
+      animation-duration: 6s;
     }
     &:nth-of-type(3) {
       animation-name: ${drop3};
+      animation-delay: 3s;
+      animation-duration: 6s;
     }
     &:nth-of-type(4) {
       animation-name: ${drop4};
+      animation-delay: 4s;
+      animation-duration: 6s;
     }
     &:nth-of-type(5) {
       animation-name: ${drop5};
+      animation-delay: 5s;
+      animation-duration: 6s;
     }
     &:nth-of-type(6) {
       animation-name: ${drop6};
+      animation-delay: 6s;
+      animation-duration: 6s;
+    }
+  }
+  div {
+    align-self: self-end;
+    color: ${({ $colors }) => $colors.fontColor};
+    @media ${({ theme }) => theme.minWidth.sm} {
+      grid-row: 1/2;
+    }
+    a {
+      display: block;
+      margin-top: 15px;
+    }
+    &:nth-of-type(1) {
+      grid-column: 1 / 4;
+      grid-row: 2/3;
+      @media ${({ theme }) => theme.minWidth.sm} {
+        grid-column: 1 / 2;
+        grid-row: 1/2;
+      }
+    }
+    &:nth-of-type(2) {
+      grid-column: 4 / 6;
+      grid-row: 2/3;
+
+      @media ${({ theme }) => theme.minWidth.sm} {
+        grid-column: 2 / 3;
+        grid-row: 1/2;
+      }
+    }
+    &:nth-of-type(3) {
+      grid-column: 5 / 7;
+      grid-row: 2/3;
+      justify-self: end;
+      @media ${({ theme }) => theme.minWidth.sm} {
+        grid-column: 3 / 4;
+        grid-row: 1/2;
+      }
     }
   }
 `;
@@ -112,7 +167,7 @@ export default function Footer({ colors, noAnimation }) {
   }, []);
 
   return (
-    <StyledFooter className="grid" ref={ref}>
+    <StyledFooter className="grid" ref={ref} $colors={colors}>
       <aside></aside>
       <aside></aside>
       <aside></aside>
@@ -120,7 +175,7 @@ export default function Footer({ colors, noAnimation }) {
       <aside></aside>
       <aside></aside>
 
-      <div>
+      {/* <div>
         <a href="">contact@cascade.fr</a>
         <a href="">+33 (0)6 74 62 64 76</a>
       </div>
@@ -131,7 +186,7 @@ export default function Footer({ colors, noAnimation }) {
       <div>
         <a href="">LinkedIn</a>
         <a href="">Twitter</a>
-      </div>
+      </div> */}
     </StyledFooter>
   );
 }
