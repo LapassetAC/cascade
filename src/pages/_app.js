7891dff2e386f0d67c6b588b09092cbe429bc4eb
AdrienLapasset <aflapasset@gmail.com>
2024-04-17 17:25:14 +0200

    fix favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -84,7 +84,24 @@ export default function App({ Component, pageProps }) {
           />
           <meta property="og:image:alt" content="Cascade" />
           <meta property="twitter:image:alt" content="Cascade" />
-          <link rel="shortcut icon" href="/favicon.jpg" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <div className={moderat.className}>
           <Layout isFromPage={isFromPage}>
