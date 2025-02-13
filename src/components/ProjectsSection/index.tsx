16422c1d2c18fe2690b409ae102de983438c3915
adrienLapasset <aflapasset@gmail.com>
2025-02-13 16:52:38 +0100

    add sanity images

--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -1,12 +1,28 @@
 import { Project } from "@/types/project";
+import Image from "next/image";
 
 const ProjectsSection = ({ projects }: { projects: Project[] }) => {
   // console.log(projects);
   return (
     <div>
-      {projects.map((project) => (
-        <div key={project._id}>{project.title}</div>
-      ))}
+      {projects.map((project) => {
+        const { title, image, url, videoUrl } = project;
+        return (
+          <div key={project._id}>
+            <Image
+              src={image.asset.url}
+              // ref={refImage}
+              fill
+              sizes="(max-width: 768px) 100vw, 800px"
+              // width={800}
+              // height={447}
+              quality={85}
+              alt={title}
+              // priority={priority}
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
