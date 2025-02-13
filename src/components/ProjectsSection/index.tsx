0b9918979789ce2bfa2711d1b91dbb78804ea3cb
adrienLapasset <aflapasset@gmail.com>
2025-02-13 17:35:14 +0100

    add video animation on hover

--- src/components/ProjectsSection/index.tsx
+++ src/components/ProjectsSection/index.tsx
@@ -1,7 +1,34 @@
 import { Project } from "@/types/project";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 
 const ProjectsSection = ({ projects }: { projects: Project[] }) => {
+  const [isMobile, setIsMobile] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      screenWidth < 768 ? setIsMobile(true) : setIsMobile(false);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleMouseEnter = () => {
+    if (!isMobile && videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+  const handleMouseLeave = () => {
+    if (!isMobile && videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+
   return (
     <div className="grid gap-8">
       {/* <div> */}
@@ -9,16 +36,43 @@ const ProjectsSection = ({ projects }: { projects: Project[] }) => {
         const { title, image, url, videoUrl } = project;
         return (
           <div className="col-span-3" key={project._id}>
-            <Image
-              src={image.asset.url}
-              // ref={refImage}
-              sizes="(max-width: 768px) 100px, 800px"
-              width={800}
-              height={447}
-              quality={85}
-              alt={title}
-              // priority={priority}
-            />
+            <a
+              href=""
+              className="group"
+              onMouseEnter={() => {
+                handleMouseEnter();
+              }}
+              onMouseLeave={() => {
+                handleMouseLeave();
+              }}
+            >
+              <Image
+                className="absolute"
+                src={image.asset.url}
+                // ref={refImage}
+                sizes="(max-width: 768px) 100px, 800px"
+                width={800}
+                height={447}
+                quality={85}
+                alt={title}
+                // priority={priority}
+              />
+
+              <div className="overflow-hidden">
+                <video
+                  ref={videoRef}
+                  className="hidden sm:block group-hover:translate-y-0 transition-transform w-full relative p-8 md:transition-transform md:transform md:-translate-y-full"
+                  preload="auto"
+                  playsInline
+                  loop
+                  muted
+                  autoPlay
+                >
+                  <source src={videoUrl} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            </a>
           </div>
         );
       })}
