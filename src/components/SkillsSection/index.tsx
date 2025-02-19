478ff5a97f2fd3c014f7bf1eda7b9043be73d7aa
adrienLapasset <aflapasset@gmail.com>
2025-02-19 12:49:48 +0100

    Update SkillsSection background with noise textures and dark mode support

--- src/components/SkillsSection/index.tsx
+++ src/components/SkillsSection/index.tsx
@@ -2,7 +2,7 @@ import SkillsLogo from "./SkillsLogo";
 
 const SkillsSection = () => {
   return (
-    <section className="grid grid-cols-5 gap-8 px-8 py-32 bg-[var(--foreground-color)] text-[var(--background-color)]">
+    <section className="grid grid-cols-5 gap-8 px-8 py-32 bg-[url('/imgs/blue-noise.jpg')] dark:bg-[url('/imgs/white-noise.jpg')] bg-repeat text-[#fcfaf7] dark:text-[#0b0bca]">
       <div className="col-start-2 col-span-2 mb-16">
         <h2 className="title mb-10">Revitalisons votre présence digitale.</h2>
         <p>
