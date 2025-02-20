eaa2f2496e7a8156b5291a30cd796094718dba0e
adrienLapasset <aflapasset@gmail.com>
2025-02-20 14:46:53 +0100

    fix for vercel build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { client } from "@/sanity/lib/client";
+import { createClient } from "@sanity/client";
 import { PROJECTS_QUERY } from "@/sanity/lib/queries";
 import ProjectsSection from "@/components/ProjectsSection";
 import { Project } from "@/types/project";
@@ -77,6 +77,13 @@ export default function Home({ projects }: { projects: Project[] }) {
 }
 
 export const getStaticProps = async () => {
+  const client = createClient({
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    apiVersion: "2024-03-19",
+    useCdn: true,
+  });
+
   const projects = await client.fetch(PROJECTS_QUERY);
 
   return {
