0015fdc3e51a0768cf41d29566a516a54ebcbaa7
adrienLapasset <aflapasset@gmail.com>
2025-02-25 17:49:31 +0100

    Refactor Tailwind config and update project styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,25 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blue: "var(--blue)",
+      },
+      fontFamily: {
+        moderat: ["var(--font-moderat)"],
+      },
+      keyframes: {
+        textAppear: {
+          "0%": {
+            transform: "translateY(-50px)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        textAppear: "textAppear 0.4s ease-out forwards",
       },
     },
   },
