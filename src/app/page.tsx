6664fbd2ea66b762353123e639d435301bb34b50
Clément Lapasset <clement.lapasset@gmail.com>
2025-07-15 15:53:20 +0200

    fix sanity build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,8 @@
-import { createClient } from "@sanity/client";
 import { PROJECTS_QUERY } from "@/sanity/lib/queries";
+import { client } from "@/sanity/lib/client";
 import HomeClient from "@/components/HomeClient";
 
 export default async function Home() {
-  const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: "2024-03-19",
-    useCdn: true,
-  });
-
   const projects = await client.fetch(PROJECTS_QUERY);
 
   return <HomeClient projects={projects} />;
