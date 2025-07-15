49deb45f905fe2547eaf1763974046000260c573
Clément Lapasset <clement.lapasset@gmail.com>
2025-07-15 16:13:45 +0200

    fix build error sanity

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -1,13 +1,9 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity";
-import { client } from './client'
+// Temporary workaround: Using regular client instead of live due to type conflicts
+// TODO: Re-enable live functionality once next-sanity types are compatible
+import { client } from "./client";
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
-});
+// Fallback to regular client.fetch instead of live sanityFetch
+export const sanityFetch = client.fetch.bind(client);
+
+// Placeholder SanityLive component
+export const SanityLive = () => null;
